a0cbd12465b65d8bc7d31ee378b10b05600be9ce
Mazen Alotaibi <32370973+mazen2886@users.noreply.github.com>
2018-06-21 04:50:16 +0300

    Initial Commit

---
+++ MBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MBox
+//
+//  Created by Mazen Alotaibi on 6/21/18.
+//  Copyright © 2018 Mazen Alotaibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
